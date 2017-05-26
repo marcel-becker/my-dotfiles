83c4da3461928a19db10639dc692d171b968c576
Marcel Becker <marcelbecker@gmail.com>
2017-05-25 20:51:28 -0700

    Added spacemacs init file.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,34 @@
-;;; Add the code below to your ~/.emacs.d/init.el to load 
-;;; the shared version of the init file.
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
+(setq use-spacemacs t)   ; or nil
+
+(when use-spacemacs
+  (setq user-emacs-directory
+        (if (eq system-type 'windows-nt)      ; Windows
+            (cond ((file-exists-p "C:/Dropbox/.spacemacs.d")
+                   "C:/Dropbox/.emacs.d/")
+                  ((file-exists-p "D:/Dropbox/.spacemacs.d")
+                   "D:/Dropbox/.spacemacs.d/")
+                  (t
+                   (expand-file-name "~/.spacemacs.d")))
+          (cond ((file-exists-p  "~/Dropbox/.spacemacs.d")
+                 "~/Dropbox/.spacemacs.d/")
+                (t
+                 (expand-file-name "~/.spacemacs.d/")))))
+  (if (file-exists-p (expand-file-name "init.el" user-emacs-directory))
+      (load (expand-file-name "init.el" user-emacs-directory))))
+
+
+;;; Add the code below to your ~/.emacs.d/init.el to load the shared version of the
+;;; init file.
 ;;;
+(unless use-spacemacs
 (defvar marcel-lisp-dir
-(if (eq system-type 'windows-nt)      ; Windows
+  (if (eq system-type 'windows-nt)      ; Windows
     (cond ((file-exists-p "C:/Dropbox/.emacs.d")
           "C:/Dropbox/.emacs.d")
           ((file-exists-p "D:/Dropbox/.emacs.d")
@@ -17,4 +43,4 @@
 
 (setq user-init-file (expand-file-name "init.el" marcel-lisp-dir))
 (if (not (eq user-init-file (expand-file-name "~/.emacs.d")))
-    (load-file user-init-file))
+    (load-file user-init-file)))
